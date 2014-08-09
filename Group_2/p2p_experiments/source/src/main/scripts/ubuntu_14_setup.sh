dc3978bb1020cf91e01ff37ed4493d137b791186
masih <masiih@gmail.com>
2014-08-09 15:18:33 +0100

    Added Ubuntu setup file

---
+++ Group_2/p2p_experiments/source/src/main/scripts/ubuntu_14_setup.sh
@@ -0,0 +1,7 @@
+
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+sudo apt-get install maven
+sudo apt-get install git
